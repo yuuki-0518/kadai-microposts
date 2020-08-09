8443f095819f05225759f75ddfbda285276fd8de
yuki.hagino <yuki051803@gmail.com>
2020-08-09 02:24:54 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/', function () {
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
