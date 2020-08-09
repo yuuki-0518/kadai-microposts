bc6908cacc608ee1d88a01a99bfb4cf96c1b496d
yuki.hagino <yuki051803@gmail.com>
2020-08-09 01:20:52 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
