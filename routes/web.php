60c0f6dc0dc15e7bc0d5442ec66c6791b275ab88
yuki.hagino <yuki051803@gmail.com>
2020-08-09 03:02:18 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 // 認証
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -25,4 +23,5 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
