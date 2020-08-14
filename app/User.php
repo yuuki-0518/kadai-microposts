1a7855f517259e882c9ea87cb91f5cea6dc435e2
yuki.hagino <yuki051803@gmail.com>
2020-08-14 08:17:57 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -120,4 +120,17 @@ class User extends Authenticatable
     {
         $this->loadCount('microposts','followings', 'followers');
     }
+    
+     /**
+     * このユーザとフォロー中ユーザの投稿に絞り込む。
+     */
+    public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 }
